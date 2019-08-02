059a5cbecbcbc96525263eb099ba0e9b828f956a
sergio <stortosa01@gmail.com>
2019-08-02 13:17:24 +0200

    heroku va5

---
+++ server/public/precache-manifest.c2fc2736f113c194b705efddaeb1a970.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32fd492631a3773892393e93ea868d85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12fed481d6fc0c9948cd",
+    "url": "/static/css/main.7120a4f8.chunk.css"
+  },
+  {
+    "revision": "87de6268639817e03688",
+    "url": "/static/js/2.28c2d404.chunk.js"
+  },
+  {
+    "revision": "12fed481d6fc0c9948cd",
+    "url": "/static/js/main.48e04612.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- server/public/service-worker.js
+++ server/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ea81e613e067a109e210c44131031c00.js"
+  "/precache-manifest.c2fc2736f113c194b705efddaeb1a970.js"
 );
 
 self.addEventListener('message', (event) => {
